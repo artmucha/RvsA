b15743e43174799dae4252bfd8edb064777ba904
Artur <artmucha@bitbucket.org>
2021-03-22 18:50:52 +0100

    add defenders

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,8 @@ canvas.height = 600;
 const cellSize = 100;
 const cellGap = 3;
 const gameGrid = [];
+const defenders = [];
+let numberOfResources = 300;
 
 // mouse
 const mouse = {
@@ -78,17 +80,66 @@ const handleGameGrid = () => {
 // projectiles
 
 // defenders
+class Defender {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = cellSize;
+    this.height = cellSize;
+    this.shooting = false;
+    this.health = 100;
+    this.projectiles = [];
+    this.timer = 0;
+  };
+  draw() {
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.fillStyle = 'gold';
+    ctx.font = '20px Arial';
+    ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 25);
+  };
+};
+
+canvas.addEventListener('click', () => {
+  const gridPositionX = mouse.x - (mouse.x % cellSize);
+  const gridPositionY = mouse.y - (mouse.y % cellSize);
+  if(gridPositionY < cellSize) return;
+
+  for (let i = 0; i < defenders.length; i++) {
+    if (defenders[i].x === gridPositionX && defenders[i].y === gridPositionY) return;
+  };
+  
+  let defenderCost = 100;
+  if(numberOfResources >= defenderCost) {
+    defenders.push(new Defender(gridPositionX, gridPositionY));
+    numberOfResources -= defenderCost;
+  };
+});
+
+const handleDefenders = () => {
+  for (let i = 0; i < defenders.length; i++) {
+    defenders[i].draw();
+  };
+};
 
 // enemies
 
 // resources
 
 // utilities
+const handleGameStatus = () => {
+  ctx.fillStyle = 'white';
+  ctx.font = '30px Arial';
+  ctx.fillText('Resources: ' + numberOfResources, 20, 50);
+};
+
 const animate = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = 'blue';
   ctx.fillRect(0, 0, controlsBar.width, controlsBar.height);
   handleGameGrid();
+  handleDefenders();
+  handleGameStatus();
   requestAnimationFrame(animate);
 };
 
